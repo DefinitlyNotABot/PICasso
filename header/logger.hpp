ca0abf41349b7abfe41781928f81210e6572a266
DefinitlyNotABot <nik.wachsmann@gmail.com>
2025-10-23 13:19:34 +0200

    added logger

---
+++ header/logger.hpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+
+class Logger
+{
+    private:
+        static const std::string filename;
+        static int loglevel;
+        static bool consoleOutput;
+
+        std::string loggerID;
+    
+    public:
+        Logger(std::string name);
+        void log(std::string message, int loglvl = 1);
+        void log(char* message, int loglvl = 1);
+
+        void setLogLevel(int loglvl);
+        void setConsoleOutput(bool enableOutput);
+};
---
+++ src/logger.cpp
@@ -0,0 +1,30 @@
+#include "../header/logger.hpp"
+
+const std::string Logger::filename = "PICasso.log";
+int Logger::loglevel = 1;
+bool Logger::consoleOutput = true;
+
+
+Logger::Logger(std::string name)
+{
+    loggerID = name;
+}
+
+void Logger::log(char* message, int loglvl)
+{
+    std::string s = message;
+    log(s, loglvl);
+}
+
+void Logger::log(std::string message, int loglvl)
+{
+    if(loglvl < loglevel){
+        return;
+    }
+
+    std::string log = "[" + loggerID + "] -> " + message;
+
+    if(consoleOutput){
+        std::cout << log << std::endl;
+    }
+}
