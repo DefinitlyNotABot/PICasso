fc63a17c0979d0ccd6b0a1a5f224eac2a5a5b690
KontraPRO <stahlben33@gmail.com>
2026-02-17 11:07:40 +0100

    added gui thread

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,37 @@
 #include "instruction.hpp"
 #include "program.hpp"
 #include "pic.hpp"
+#include "TerminalGUI.hpp"
 
 #include "alu.hpp"
 #include <iostream>
 #include <thread>
 
-void picRuntimeThread(PIC& pic) {
+void picRuntimeThread(PIC &pic)
+{
     pic.run();
+}
+
+void guiRuntimeThread(TerminalGUI &gui)
+{
+    gui.loop();
 }
 
 int main()
 {
+    TerminalGUI gui;
+
+    std::thread guiThread(guiRuntimeThread, std::ref(gui));
 
     PIC pic;
     pic.loadProgram("progs/TPicSim1.LST");
 
     std::cout << "Program loaded, starting execution..." << std::endl;
 
-    std::thread t(picRuntimeThread, std::ref(pic));
-    t.join();
+    std::thread picThread(picRuntimeThread, std::ref(pic));
+
+    picThread.join();
+    guiThread.join();
 
     return 0;
 }
