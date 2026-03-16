36b89cdb54ab44256cdfaaf5546a71ddd61192fe
KontraPRO <stahlben33@gmail.com>
2026-03-16 11:09:57 +0100

    added class for interuppts

---
+++ src/interrups.cpp
@@ -0,0 +1,46 @@
+#include "interrupts.hpp"
+
+void Interrupts::checkINTCON()
+{
+    uint8_t intconValue = memoryInterface->readRegister(INTCON_REGISTER);
+    this->flags = *reinterpret_cast<InterruptFlags *>(&intconValue);
+}
+
+void Interrupts::gotInterrupted()
+{
+    memoryInterface->writeBit(INTCON_REGISTER, GIE_BIT, 0);
+    memoryInterface->pushToStack(memoryInterface->getProgramCounter());
+    memoryInterface->setProgramCounter(0x04);
+}
+
+Interrupts::Interrupts(std::shared_ptr<MemoryInterface> memoryInterface) : memoryInterface(memoryInterface)
+{
+}
+
+Interrupts::~Interrupts()
+{
+}
+
+uint8_t Interrupts::checkInterrupts()
+{
+    checkINTCON();
+
+    if (flags.GIE)
+    {
+        if (flags.EEIE)
+        {
+        }
+        if (flags.T0IE & flags.T0IF)
+        {
+            gotInterrupted();
+        }
+        if (flags.INTE & flags.INTF)
+        {
+            gotInterrupted();
+        }
+        if (flags.RBIE & flags.RBIF)
+        {
+            gotInterrupted();
+        }
+    }
+}
